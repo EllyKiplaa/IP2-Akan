8dea1ed72a7f713ee401b9b25e65b9352c177f9f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-05-03 14:46:38 +0300

    I styled my website headers

---
+++ index.js
@@ -0,0 +1,28 @@
+var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function getuserInfo(){
+var day = parseInt(document.getElementById("day").value);
+
+var month = parseInt(document.getElementById("month").value);
+
+var year = parseInt(document.getElementById("year").value);
+
+var female = document .getElementById("male").value;
+
+var female = document .getElementById("male").value;
+
+if(day <= || day >31){
+    alert("Enter valid day")
+}
+else if(month <= || month >12 ||(month == 2 && day >29)){
+    alert ("Enter valid month..")
+}
+
+if (male.checked == trur){
+    
+    else if (female.checked == true)
+}
